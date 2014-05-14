dbc394e40cecad29d68ce7af070c14143f9a83dc
Michael Tirenin <michaeltirenin@Michaels-MacBook-Pro-2.local>
2014-05-14 09:42:28 -0700

    Initial Commit

---
+++ Assignment ExtClassFunc/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  Assignment ExtClassFunc
+//
+//  Created by Michael Tirenin on 5/14/14.
+//  Copyright (c) 2014 Michael Tirenin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTViewController : UIViewController
+
+@end
